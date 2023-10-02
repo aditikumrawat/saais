2f3c8b21ee92db69e7e2346e1dee388251c3f76a
Aditi Kumrawat <aditikumrawat24@gmail.com>
2023-10-02 11:04:30 +0530

    Successfully added all the crud operations

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-from pymongo.mongo_client import MongoClient
 from fastapi import FastAPI
 from route.routes import router
+from pymongo.mongo_client import MongoClient
+# from pymongo.server_api import ServerApi
 
 app = FastAPI()
 
 app.include_router(router)
+
+# uri = "mongodb+srv://saais_admin:ZA81qfDASREEZ5qD@saaisdb.dyhcbgz.mongodb.net/?retryWrites=true&w=majority"
+
+# client = MongoClient(uri)
+
+# try:
+#     client.admin.command('ping')
+#     print("Pinged your deployment. You successfully connected to MongoDB!")
+# except Exception as e:
+#     print(e)
--- route/routes.py
+++ route/routes.py
@@ -6,9 +6,44 @@ from bson import ObjectId
 
 router = APIRouter()
 
+@router.get('/')
+async def home():
+    return {
+        "success" : "Welcome to the home page!"
+    }
 
 # Get request method
-@router.get('/')
-async def get_user():
+@router.get('/users/')
+async def get_users():
     users = list_User(collection_name.find())
     return users
+
+
+@router.get('/users/{id}')
+async def get_user_with_id(id: str):
+    users = list_User(collection_name.find())
+    for user in users:
+        if user["user_id"] == id:
+            return {
+                    "user": user
+                }
+    return {
+            "error": "User does not exits"
+        }
+
+
+@router.post('/users/add_user')
+async def create_user(user: User):
+    collection_name.insert_one(dict(user))
+
+
+@router.put('/users/update_user/{id}')
+async def update_user(id: str, user: User):
+    collection_name.find_one_and_update(
+        {"_id": ObjectId(id)}, {"$set": dict(user)})
+
+@router.delete('/delete/users')
+async def delete_users():
+      collection_name.delete_many
+
+
